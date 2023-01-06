18d577afa7cd881164dd526f6c61db59a5bfdc27
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-06 11:16:43 +0100

    added multer and multer configuration

---
+++ routes/multer.js
@@ -0,0 +1,17 @@
+//Multer Configuration
+const multer = require("multer")
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        const __dir = path.join(__dirname, "../public/uploads")
+        cb(null, __dir)
+    },
+    filename:function(req, file, cb){
+        const fileName = file.originalname.toLowerCase()
+        cb(null, fileName)
+    }
+})
+const upload = multer({storage})
+
+module.exports = upload
