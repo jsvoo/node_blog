62289576a2635d859e25132f297d3a9e7ea5fa15
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-06 13:37:54 +0100

    added route for comments endpoints

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path")
 const bodyParser = require("body-parser")
 const users = require("./routes/users")
 const posts = require("./routes/posts")
+const comments = require("./routes/comments")
 const mongoose = require("mongoose")
 const port = process.env.PORT || 5050
 const router = express.Router()
@@ -22,6 +23,7 @@ app.use(bodyParser.json())
 app.use("/", welcome)
 app.use("/users", users)
 app.use("/posts", posts) 
+app.use("/comments", comments) 
 
 
 mongoose.connect("mongodb://localhost:27017/Node_blog", { useNewUrlParser: true, useUnifiedTopology: true }, 
