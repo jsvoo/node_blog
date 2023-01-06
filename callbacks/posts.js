c0948b71a46ae65e8d717b8de9d14beeb82a6f88
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-06 11:13:53 +0100

    added funtions to get all posts and create new post

--- callbacks/posts.js
+++ callbacks/posts.js
@@ -1,4 +1,17 @@
+
+
+const postModel = require("../models/posts/posts")
+
 exports.postHome = (req, res)=>{
     res.send("posts route")
 }
  
+exports.allPosts = async (req, res)=>{
+    const allPosts = await postModel.find().lean()
+    res.send(allPosts)
+}
+
+exports.createPost = async (req, res)=>{
+    const post = await postModel.create(req.body)
+    res.send(post)
+}
