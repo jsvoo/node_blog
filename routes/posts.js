065d3a0b98139c97339b53d97ab7d85579d3477b
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-06 11:57:13 +0100

    added routes to get single post, create single post and delete single post

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require("express")
-const { postHome, allPosts, createPost } = require("../callbacks/posts")
+const { postHome, allPosts, createPost, singlePost, deletePost } = require("../callbacks/posts") 
 const router = express.Router()
 
 router.get("/", postHome)
 
 router.get("/posts", allPosts)
+router.get("/post:post_id", singlePost)
 router.post("/post", createPost)
-
+router.delete("/post:post_id", deletePost)
 
 
 module.exports = router 
