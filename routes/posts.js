45b5db858f3e43b71fc1ba549af2f0f386f13834
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-06 11:22:53 +0100

    added post route to create posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { postHome, allPosts } = require("../callbacks/posts")
+const { postHome, allPosts, createPost } = require("../callbacks/posts")
 const router = express.Router()
 
 router.get("/", postHome)
 
 router.get("/posts", allPosts)
+router.post("/post", createPost)
 
 
 
