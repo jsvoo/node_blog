537fe1ffcb71f79816de84ecc0de01bfb9eef384
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-10 23:52:59 +0100

    added route to get user's posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { postHome, allPosts, createPost, singlePost, deletePost, updatePost } = require("../callbacks/posts") 
+const { postHome, allPosts, createPost, singlePost, deletePost, updatePost, usersPosts } = require("../callbacks/posts") 
 const upload = require("./multer")
 const router = express.Router()
 
@@ -7,6 +7,7 @@ router.get("/", postHome)
 
 router.get("/all", allPosts)
 router.get("/post:post_id", singlePost)
+router.get("/user/posts:/user_id", usersPosts)
 router.post("/create", upload.any(), createPost)
 router.delete("/post:post_id", deletePost)
 router.put("/update", upload.any(), updatePost)
